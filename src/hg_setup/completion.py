bab7258dfc4b5950f47da2b5973c3f851453587a
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-12-14 23:21:54 +0100

    Short note on completion

---
+++ src/hg_setup/completion.py
@@ -0,0 +1,48 @@
+"""Shell completion
+
+bash
+----
+
+Mercurial bash completion file (called `hg`) can be in:
+
+- /etc/bash_completion.d
+- /usr/share/bash-completion/completions
+- ~/.local/share/bash-completion/completions
+
+zsh
+---
+
+Mercurial zsh completion file (called `_hg`) can be in:
+
+- /usr/share/zsh/site-functions
+- /usr/share/zsh/*/functions
+- ~/.local/share/zsh/site-functions
+- ~/.zfunc
+- $ZSH_CUSTOM/plugins/hg
+
+tcsh
+----
+
+???
+
+Powershell
+----------
+
+???
+
+fish
+----
+
+- ~/.config/fish/completions
+
+elvish
+------
+
+???
+
+xonsh
+-----
+
+Use bash completion + other stuff.
+
+"""
