34abd6362e1b4f83f59ba5a59ce00068b22d1d01
Pierre Augier <pierre.augier@univ-grenoble-alpes.fr>
2024-12-18 09:41:52 +0100

    CI Windows: try to avoid what is broken

--- .github/debug_which_hg.py
+++ .github/debug_which_hg.py
@@ -10,3 +10,8 @@ hg = Path(which("hg"))
 print(hg)
 print(hg.read_text(encoding="utf-8"))
 print(sorted(hg.parent.glob("*")))
+
+hg_py = hg.with_name("hg")
+if hg_py.exists():
+    print(hg_py)
+    print(hg_py.read_text(encoding="utf-8"))
